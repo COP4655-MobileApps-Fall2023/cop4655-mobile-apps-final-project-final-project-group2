574655d5f2c5578d06ae488216255581dc6305b6
tbender2021 <143292915+tbender2021@users.noreply.github.com>
2023-12-05 21:15:54 -0500

    Add files via upload

---
+++ mainViewController.swift
@@ -0,0 +1,112 @@
+
+import Foundation
+import UIKit
+var eventsList = [Event]()
+
+class mainViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource
+{
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setCellsView()
+        setMonthView()
+        print(eventsList)
+    }
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    @IBOutlet weak var monthLabel: UILabel!
+    
+    @IBAction func previousMonth(_ sender: Any) {
+        
+        selectedDate = CalendarHelper().minusMonth(date: selectedDate)
+        setMonthView()
+    }
+    
+    
+    
+    @IBAction func nextMonth(_ sender: Any)
+    {
+        selectedDate = CalendarHelper().plusMonth(date: selectedDate)
+        setMonthView()
+                
+    }
+    
+    //prevent screen from rotating
+    
+    override open var shouldAutorotate: Bool
+    {
+        return false
+    }
+    
+    //var selectedDate = Date()
+    var totalSquares = [String]()
+    
+    func setCellsView()
+    {
+        let width = (collectionView.frame.size.width - 2) / 8
+        let height = (collectionView.frame.size.height - 2) / 8
+        
+        let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.itemSize = CGSize (width: width, height: height )
+        
+    }
+    
+    func setMonthView()
+    {
+        totalSquares.removeAll()
+        
+        let daysInMonth = CalendarHelper().daysInMonth(date: selectedDate)
+        let firstDayOfMonth = CalendarHelper().firstOfMonth(date: selectedDate)
+        let startingSpaces = CalendarHelper().weekDay(date: firstDayOfMonth)
+        
+        var count: Int = 1
+        
+        while (count <= 42)
+        {
+            if(count <= startingSpaces || count - startingSpaces > daysInMonth)
+            {
+                totalSquares.append("")
+            }
+            else
+            {
+                totalSquares.append(String(count - startingSpaces))
+            }
+            count += 1
+            
+        }
+        monthLabel.text = CalendarHelper().monthString(date: selectedDate) + " " + CalendarHelper().yearString(date: selectedDate)
+        collectionView.reloadData()
+    }
+    
+    @IBAction func logOutTapped(_ sender: Any) {
+        showConfirmLogoutAlert()
+    }
+    
+    
+    
+    private func showConfirmLogoutAlert() {
+        let alertController = UIAlertController(title: "Are you sure you want to Log out of \(User.current?.username ?? "current account")'s Account?", message: nil, preferredStyle: .alert)
+        let logOutAction = UIAlertAction(title: "Log out", style: .destructive) { _ in
+            NotificationCenter.default.post(name: Notification.Name("logout"), object: nil)
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alertController.addAction(logOutAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        totalSquares.count
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "calCell", for: indexPath) as! CalendarCell
+        
+        cell.dayOfMonth.text = totalSquares[indexPath.item]
+        return cell
+    }
+}
